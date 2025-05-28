b10badad267f3b0449916c07884b03f46215f1ea
Connor <alleconn@oregonstate.edu>
2025-05-28 02:51:51 -0700

    finished with no error handling

--- rp.s
+++ rp.s
@@ -1,4 +1,4 @@
-/* UNFINISHED, SEG FAULTS
+/* WORKING, WITH NO ERROR HANDLING
  * Reverse polish calculator
  * Author: Connor Allen 934467445
  */
@@ -15,142 +15,124 @@ main:
 	push %rbp
 	mov %rsp, %rbp
 
+	cmp $1, %rdi
+	jle _end	
+	sub $1024, %rsp
+	mov %rsp, %rbx
+
 	mov %rdi, %r13
 	mov %rsi, %r14
 
 	mov $0, %r15
-	addq $8, %r14
+	add $8, %r14
+
 	jmp parsing_loop
 
 parsing_loop:
-	leaq 8(%rsi), %rsi
-	movq (%rsi), %r12
+	mov (%r14), %r12
 	test %r12, %r12
 	je print_result
-	addq $8, %rsi
+	add $8, %r14
 
 add_cmp:
-	push %rsi
-
 	movq %r12, %rsi
 	leaq op_add(%rip), %rdi
-	
-	movq %rsp, %r13
-	andq $-16, %rsp
 
 	xor %rax, %rax
 	call strcmp
-
-	movq %r13, %rsp
-	pop %rsi 
 
 	test %eax, %eax
 	jne sub_cmp
 
-	pop %r8
-	addq %r8, (%rsp)
+	sub $8, %rbx
+	mov (%rbx), %r8
+	sub $8, %rbx
+	add %r8, (%rbx)
+	add $8, %rbx
 	jmp parsing_loop
 
 sub_cmp:
-	push %rsi
 	movq %r12, %rsi
 	leaq op_sub(%rip), %rdi
 
-	movq %rsp, %r13
-	andq $-16, %rsp
-
 	xor %rax, %rax
 	call strcmp
-
-	movq %r13, %rsp
-	pop %rsi
 
 	test %eax, %eax
 	jne mul_cmp
 
-	pop %r8
-	subq %r8, (%rsp)
+	sub $8, %rbx
+	mov (%rbx), %r8
+	sub $8, %rbx
+	mov (%rbx), %rax
+	sub %r8, %rax
+	mov %rax, (%rbx)
+	add $8, %rbx
 	jmp parsing_loop
 
 mul_cmp:
-	push %rsi
 	movq %r12, %rsi
 	leaq op_mul(%rip), %rdi
-	
-	movq %rsp, %r13
-	andq $-16, %rsp
 
 	xor %rax, %rax
 	call strcmp
 
-	movq %r13, %rsp
-	pop %rsi
 	test %eax, %eax
 	jne div_cmp
 
-	pop %rax
-	pop %r8
-	imulq %r8
-	push %rax
+	sub $8, %rbx
+	mov (%rbx), %rax
+	sub $8, %rbx
+	imulq (%rbx), %rax
+	mov %rax, (%rbx)
+	add $8, %rbx
 	jmp parsing_loop
 
 div_cmp:
-	push %rsi
 	movq %r12, %rsi
 	leaq op_div(%rip), %rdi
-	
-	movq %rsp, %r13
-	andq $-16, %rsp
 
 	xor %rax, %rax
 	call strcmp
-
-	movq %r13, %rsp
-	pop %rsi
 
 	test %eax, %eax
 	jne num_convert
 
-	pop %r8
-	pop %rax
+	sub $8, %rbx
+	mov (%rbx), %r8
+	sub $8, %rbx
+	mov (%rbx), %rax
 	cqo
 	idivq %r8
-	push %rax
+	mov %rax, (%rbx)
+	add $8, %rbx
 	jmp parsing_loop
 
+
 num_convert:
-	push %rsi
 	movq %r12, %rdi
 	leaq endptr(%rip), %rsi
 	xor %rdx, %rdx
 
-	movq %rsp, %r13
-	andq $-16, %rsp
-
-	xor %rax, %rax
 	call strtoll
 
-	movq %r13, %rsp
-	pop %rsi
-
-	push %rax
-	
+	mov %rax, (%rbx)
+	add $8, %rbx
 	jmp parsing_loop
 
 print_result:
+	sub $8, %rbx
+	mov (%rbx), %rsi
 	leaq print_result_str(%rip), %rdi
-	pop %rsi
 
-	movq (%rsp), %rdi
 	xor %rax, %rax
 	call printf
 	
-	leaq 8(%rsp), %rsp
 	leave
 	ret
-
-exit:
-	movq $SYS_exit, (%rax)
+_end:
+	mov $0, %rdi
+	mov $SYS_exit, %rax
 	syscall
 
 .section .rodata
